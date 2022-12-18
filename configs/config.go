24f1338343dff043d155086f2f71a4181455e897
Giovane <giovaneaalmeida27@gmail.com>
2022-12-18 15:41:23 -0300

    feat: add jwt configuration

--- configs/config.go
+++ configs/config.go
@@ -1,21 +1,23 @@
 package configs
 
 import (
-	"gihub.com/go-chi/jwtauth"
+	"time"
+
+	"github.com/go-chi/jwtauth"
 	"github.com/spf13/viper"
 )
 
 type conf struct {
-	DBDriver      string
-	DBHost        string
-	DBPort        string
-	DBUser        string
-	DBPass        string
-	DBName        string
-	WebServerPort string
-	JWTSecret     string
-	JWTExpiresIn  int
-	TokenAuth     *jwtauth.JWTAuth
+	DBDriver      string           `mapstructure:"DB_DRIVER"`
+	DBHost        string           `mapstructure:"DB_HOST"`
+	DBPort        string           `mapstructure:"DB_PORT"`
+	DBUser        string           `mapstructure:"DB_USER"`
+	DBPass        string           `mapstructure:"DB_PASS"`
+	DBName        string           `mapstructure:"DB_NAME"`
+	WebServerPort string           `mapstructure:"WEB_SERVER_PORT"`
+	JWTSecret     string           `mapstructure:"JWT_SECRET"`
+	JWTExpiresIn  int              `mapstructure:"JWT_EXPIRES_IN"`
+	TokenAuth     *jwtauth.JWTAuth `mapstructure:"TOKEN_AUTH"`
 }
 
 var cfg *conf
@@ -37,5 +39,7 @@ func LoadConfig(path string) *conf {
 		panic(err)
 	}
 
+	cfg.TokenAuth = jwtauth.New("HS256", []byte(cfg.JWTSecret), nil)
+	cfg.JWTExpiresIn = int(jwtauth.ExpireIn(time.Duration(cfg.JWTExpiresIn) * time.Second))
 	return cfg
 }
