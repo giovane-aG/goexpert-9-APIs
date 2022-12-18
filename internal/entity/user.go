e6d2d016c75025697062cc88265295e581750949
Giovane <giovaneaalmeida27@gmail.com>
2022-12-18 15:52:32 -0300

    feat: create user entity

---
+++ internal/entity/user.go
@@ -0,0 +1,33 @@
+package entity
+
+import (
+	"github.com/giovane-aG/goexpert/9-APIs/pkg/entity"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID       entity.ID `json:"id"`
+	Name     string    `json:"username"`
+	Password string    `json:"password"`
+	Email    string    `json:"email"`
+}
+
+func NewUser(name, email, password string) (*User, error) {
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		panic(err)
+	}
+
+	return &User{
+		Name:     name,
+		Email:    email,
+		Password: string(hash),
+		ID:       entity.NewId(),
+	}, nil
+}
+
+func (u *User) ValidatePassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	return err == nil
+}
