80d0e4859ef4e6cb7912a819ea2b8222ee96c6bf
Giovane Aguiar <giovaneaalmeida27@gmail.com>
2022-12-22 09:35:42 -0300

    feat: create ProductInterface

--- internal/infra/database/interfaces.go
+++ internal/infra/database/interfaces.go
@@ -8,3 +8,11 @@ type UserInterface interface {
 	Create(user *entity.User) error
 	Find(id string) (*entity.User, error)
 }
+
+type ProductInterface interface {
+	Create(product *entity.Product) error
+	FindAll(page, limit int, sort string) ([]entity.Product, error)
+	FindById(id string) (*entity.Product, error)
+	Update(product *entity.Product) error
+	Delete(id string) error
+}
