51829bb93725b685986d27abaa119ee4de9cd3df
Giovane Aguiar <giovaneaalmeida27@gmail.com>
2022-12-22 09:37:31 -0300

    feat: add create

--- internal/infra/database/product_db.go
+++ internal/infra/database/product_db.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"github.com/giovane-aG/goexpert/9-APIs/internal/entity"
 	"gorm.io/gorm"
 )
 
@@ -10,4 +11,8 @@ type Product struct {
 
 func NewProduct(db *gorm.DB) *Product {
 	return &Product{DB: db}
+}
+
+func (p *Product) Create(product *entity.Product) error {
+	return p.DB.Create(p).Error
 }
