8f1317ab217d71daa1c08295f389fb012a01ea2d
Giovane <giovaneaalmeida27@gmail.com>
2023-01-01 21:34:54 -0300

    feat: creates FindByEmail function

--- internal/infra/http/user/user_controller.go
+++ internal/infra/http/user/user_controller.go
@@ -2,12 +2,14 @@ package user_controller
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 
 	"github.com/giovane-aG/goexpert/9-APIs/internal/entity"
 	"github.com/giovane-aG/goexpert/9-APIs/internal/infra/database"
 	"github.com/giovane-aG/goexpert/9-APIs/internal/infra/http/user/dtos"
+	"github.com/go-chi/chi/v5"
 )
 
 type UserController struct {
@@ -23,31 +25,63 @@ func (c *UserController) CreateUser(w http.ResponseWriter, r *http.Request) {
 
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		panic(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
-	var parsedBody *dtos.CreateUserDto
-	err = json.Unmarshal(body, &parsedBody)
+	// parsing body
+	var parsedBody *dtos.CreateUserDto = &dtos.CreateUserDto{}
+	json.Unmarshal(body, &parsedBody)
+	err = parsedBody.ValidateFields()
+
 	if err != nil {
-		panic(err)
+		message := fmt.Sprintf("message: %v", err)
+		parsedMessage, _ := json.Marshal(map[string]string{"message": message})
+
+		w.Write(parsedMessage)
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
 
+	// creating entity
 	var user *entity.User
 	user, err = entity.NewUser(parsedBody.Name, parsedBody.Email, parsedBody.Password)
 	if err != nil {
-		panic(err)
+		w.WriteHeader(http.StatusBadRequest)
 	}
 
+	// saving entity
 	err = c.UserDB.Create(user)
 
 	if err != nil {
 		response, _ := json.Marshal(map[string]string{"message": err.Error()})
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write(response)
-
+		return
 	}
 
 	response, _ := json.Marshal(map[string]string{"message": "User created successfully"})
-	w.WriteHeader(http.StatusCreated)
 	w.Write(response)
+	w.WriteHeader(http.StatusBadRequest)
+}
+
+func (c *UserController) FindByEmail(w http.ResponseWriter, r *http.Request) {
+	email := chi.URLParam(r, "email")
+	fmt.Println(email)
+
+	user, err := c.UserDB.FindByEmail(email)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	jsonResponse, err := json.Marshal(user)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(jsonResponse)
+	w.WriteHeader(http.StatusOK)
+	return
 }
