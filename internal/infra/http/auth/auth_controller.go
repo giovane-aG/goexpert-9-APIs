b578715c84c1803ab289f4fa47b234d5044de413
Giovane <giovaneaalmeida27@gmail.com>
2023-01-05 13:49:09 -0300

    feat: validates user and returns jwt token

--- internal/infra/http/auth/auth_controller.go
+++ internal/infra/http/auth/auth_controller.go
@@ -4,13 +4,27 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/giovane-aG/goexpert/9-APIs/internal/infra/database"
 	"github.com/giovane-aG/goexpert/9-APIs/internal/infra/http/auth/dtos"
+	"github.com/go-chi/jwtauth"
 )
 
-type AuthController struct{}
+type AuthController struct {
+	UserDB       *database.User
+	JwtSecret    string
+	JwtExpiresIn int
+}
 
-func NewAuthController() *AuthController {
-	return &AuthController{}
+type AcessToken struct {
+	Token string `json:"token"`
+}
+
+func NewAuthController(userDb *database.User, jwtSecret string, jwtExpiresIn int) *AuthController {
+	return &AuthController{
+		UserDB:       userDb,
+		JwtSecret:    jwtSecret,
+		JwtExpiresIn: jwtExpiresIn,
+	}
 }
 
 func (a *AuthController) Login(w http.ResponseWriter, r *http.Request) {
@@ -23,4 +37,32 @@ func (a *AuthController) Login(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(err)
 		return
 	}
+
+	user, err := a.UserDB.FindByEmail(loginDto.Email)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+
+	if !user.ValidatePassword(loginDto.Password) {
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode("incorrect password")
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	tokenAuth := jwtauth.New("HS256", []byte(a.JwtSecret), nil)
+
+	_, token, _ := tokenAuth.Encode(map[string]interface{}{
+		"user_id": user.ID.String(),
+		"email":   user.Email,
+	})
+
+	accessToken := AcessToken{
+		Token: token,
+	}
+
+	json.NewEncoder(w).Encode(accessToken)
 }
