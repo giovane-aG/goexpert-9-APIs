0f5732af7efdc5c33744f6ad7a326bc2ee82a0a9
Giovane <giovaneaalmeida27@gmail.com>
2023-01-07 11:37:05 -0300

    feat: create product dto

---
+++ internal/infra/http/product/dtos/create_product_dto.go
@@ -0,0 +1,14 @@
+package dtos
+
+import "errors"
+
+type CreateProductDto struct {
+	Name  string
+	Price float64
+}
+
+func (c *CreateProductDto) ValidateFields() error {
+	if c.Name == "" || c.Price <= 0 {
+		return errors.New("please insert valid fields")
+	}
+}
